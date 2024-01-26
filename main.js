301fce9564bde16def836dbe91bbf31db1bdd137
Samuraikiller <nurikmirr2005@gmail.com>
2024-01-26 16:39:31 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,37 @@
+document.getElementById('searchButton').addEventListener('click', searchMovie);
+
+function searchMovie() {
+    var input = document.getElementById('movieInput').value;
+    fetch('http://www.omdbapi.com/?i=tt3896198&apikey=6e786caa&s=' + input)
+        .then(response => response.json())
+        .then(data => {
+            var movieInfo = document.getElementById('movieInfo');
+            var html = `
+                <div class="movieCard">
+                    <img src="${data.Search[0].Poster}" alt="Movie Poster">
+                    <h3>${data.Search[0].Title}</h3>
+                    <p><strong>Год:</strong> ${data.Search[0].Year}</p>
+                    <p><strong>Тип:</strong> ${data.Search[0].Type}</p>
+                    <button class="addToFavorite" onclick="addToFavorite('${data.Search[0].Title}')">Добавить в избранное</button>
+                </div>
+            `;
+            movieInfo.innerHTML = html;
+        })
+        .catch(error => console.log(error));
+}
+
+function addToFavorite(movieTitle) {
+    var favoriteMovies = document.getElementById("favoriteMovies");
+    var html = `
+        <div class="movieCard">
+            <h3>${movieTitle}</h3>
+            <button class="remove" onclick="removeFromFavorite(this)">Remove</button>
+        </div>
+    `;
+    favoriteMovies.innerHTML += html;
+}
+
+function removeFromFavorite(button) {
+    var movieCard = button.parentNode;
+    movieCard.parentNode.removeChild(movieCard);
+}
